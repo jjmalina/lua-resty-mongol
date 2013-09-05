fdcd596a54cf46a340218c1285f23045ae55e727
Jeremiah Malina <jmalina327@gmail.com>
2013-09-05 13:42:32 -0400

    bson.lua: start from index 1 when converting table arrays to bson

--- lib/resty/mongol/bson.lua
+++ lib/resty/mongol/bson.lua
@@ -186,7 +186,7 @@ function to_bson(ob)
 	elseif onlyarray then
 		local r = { }
 
-		local low = 0
+		local low = 1
 		--if seen_n [ 0 ] then low = 0 end
 		for i=low , high_n do
 			r [ i ] = pack ( i , seen_n [ i ] )
